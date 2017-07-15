cac701bc493d0f438d5019eb8178949138d0f3e7
Roman Malkevych <henprokuror@gmail.com>
2017-07-15 20:30:06 +0300

    Updated main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include "./Primitives/Render.h"
+#include "Primitives/Window.h"
+#include "Primitives/Camera.h"
 
 int main()
 {
+	//std::shared_ptr<Window> window = std::make_shared<Window>();
 
+	Window *window = new Window(800, 600);
+	Camera *camera = new Camera(Vec3d(0, 0, -2000));
+	//Sphere sphere();
+	//TODO list with 3 spheres
 	return 0;
 }

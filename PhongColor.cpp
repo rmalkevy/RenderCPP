7d05acf09dd9cd7caa0d915d4d6b9d1eb518204b
Roman Malkevych <henprokuror@gmail.com>
2017-07-27 15:52:12 +0300

    Updated Phong color

--- PhongColor.cpp
+++ PhongColor.cpp
@@ -6,6 +6,16 @@
 #include "includes/Vec3d.h"
 #include "includes/Camera.h"
 
+static const float kInfinity = std::numeric_limits<float>::max();
+static const float kEpsilon = 1e-8;
+static const Vec3d kDefaultBackgroundColor = Vec3d(0.235294, 0.67451, 0.843137);
+static const double bias = 0.0001;
+
+Vec3d reflect(const Vec3d &I, const Vec3d &N)
+{
+    return I - N * 2 * I.dot(N);
+}
+
 Vec3d calulatePhongColor(Camera &camera, Vec3d hitColor)
 {
     Vec3d diffuse(0);
@@ -13,13 +23,14 @@ Vec3d calulatePhongColor(Camera &camera, Vec3d hitColor)
     Vec3d hitPoint = camera.hitPoint;
     Vec3d hitNormal = camera.hitNormal;
 
-    for (uint32_t i = 0; i < lights.size(); ++i) {
+    for (uint32_t i = 0; i < 1; ++i) // TODO i < lights.size()
+    {
         Vec3d lightDir;
         Vec3d lightIntensity;
         IsectInfo isectShad;
         lights[i]->illuminate(hitPoint, lightDir, lightIntensity, isectShad.tNear);
 
-        bool vis = !trace(hitPoint + hitNormal * options.bias, -lightDir, objects, isectShad, kShadowRay);
+        bool vis = !trace(hitPoint + hitNormal * bias, -lightDir, objects, isectShad, kShadowRay);
 
         // compute the diffuse component
         diffuse += vis * isect.hitObject->albedo * lightIntensity * std::max(0.f, hitNormal.dot(-lightDir));
